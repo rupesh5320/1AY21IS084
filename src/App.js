21cff8913b929f253791e7a1494f81b4d3993286
rupesh5320 <rupesh2998@gmail.com>
2024-07-11 03:28:21 +0530

    Node modules Added

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className='text-3xl flex-col text-center justify-center'>
-      <p className=" bg-red-400" >Hello World</p> 
+      <p className=" bg-red-400" > Hello World </p> 
     </div>
   );
 }
